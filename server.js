e8707ed1488236917e8d06963d9c4231508144f1
jhonattan ferri <jhoontutors@gmail.com>
2023-09-15 15:36:09 -0300

    0934ut

--- server.js
+++ server.js
@@ -1,37 +1,65 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000; // Use a porta fornecida pelo ambiente ou 3000 por padrão
+const port = process.env.PORT || 5000; 
 const cors = require('cors');
+const nodemailer = require('nodemailer');
+
 app.use(express.json());
 
 app.use(cors({
     origin: 'https://projeto-front-end-login-e-cadastro.vercel.app',
   }));
-// Simule um banco de dados de usuários (você deve usar um banco de dados real)
 const users = [];
 
-// Rota de registro de usuário
+const transporter = nodemailer.createTransport({
+  service: 'Gmail', 
+  auth: {
+    user: 'jhoontutors@gmail.com', 
+  },
+});
+
+app.post('/api/send-feedback', (req, res) => {
+  const { message } = req.body;
+
+  if (!message) {
+    return res.status(400).json({ message: 'Mensagem não fornecida' });
+  }
+
+  const mailOptions = {
+    from: 'jhoontutors@gmail.com', 
+    to: 'jhoontutors@gmail.com', 
+    subject: 'Nova Mensagem de Usuário',
+    text: `Nova mensagem recebida: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Ocorreu um erro ao enviar a mensagem por e-mail' });
+    } else {
+      console.log('E-mail de mensagem enviado: ' + info.response);
+      res.status(200).json({ message: 'Mensagem enviada com sucesso' });
+    }
+  });
+});
+
 app.post('/api/register', (req, res) => {
     const { username, password } = req.body;
 
-  // Verifique se o usuário já existe
   const existingUser = users.find((user) => user.username === username);
   if (existingUser) {
     return res.status(400).json({ message: 'Usuário já registrado' });
   }
 
-  // Crie um novo usuário (você deve criptografar a senha antes de armazená-la)
   const newUser = { username, password };
   users.push(newUser);
 
   return res.status(201).json({ message: 'Usuário registrado com sucesso' });
 });
 
-// Rota de login de usuário
 app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
 
-  // Verifique se o usuário existe e a senha está correta (você deve verificar contra o banco de dados)
   const user = users.find((user) => user.username === username);
 
   if (!user || user.password !== password) {
