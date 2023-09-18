323ea1e70b8367031e0679814ed1c99a300b37d8
jhonattan ferri <jhoontutors@gmail.com>
2023-09-18 15:38:59 -0300

    8yu23ryu8r

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000; 
-const prisma = new PrismaClient();
+const port = process.env.PORT || 5000;
+const { PrismaClient } = require('@prisma/client'); // Importe o PrismaClient corretamente
+const prisma = new PrismaClient(); // Crie uma instância do PrismaClient
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { PrismaClient } = require('@prisma/client');
+
 app.use(express.json());
 
-app.use(cors({
+app.use(
+  cors({
     origin: 'https://projeto-front-end-login-e-cadastro.vercel.app',
-  }));
+  })
+);
+
 const users = [];
 app.use(bodyParser.json());
 
@@ -32,9 +36,8 @@ app.post('/api/feedback', async (req, res) => {
   }
 });
 
-
 app.post('/api/register', (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
   const existingUser = users.find((user) => user.username === username);
   if (existingUser) {
@@ -48,7 +51,7 @@ app.post('/api/register', (req, res) => {
 });
 
 app.post('/api/login', (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
   const user = users.find((user) => user.username === username);
 
