15b659384a8c151496f984f3e97901086161cd24
jhonattan ferri <jhoontutors@gmail.com>
2023-09-18 13:10:13 -0300

    32wsds

--- server.js
+++ server.js
@@ -1,36 +1,35 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000; 
+const prisma = new PrismaClient();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const { PrismaClient } = require('@prisma/client');
 app.use(express.json());
 
 app.use(cors({
     origin: 'https://projeto-front-end-login-e-cadastro.vercel.app',
   }));
 const users = [];
-const feedbackData = []; 
+app.use(bodyParser.json());
 
-app.post('/api/feedback', (req, res) => {
-  const { opinion } = req.body;
+// Rota para receber avaliações
+app.post('/api/feedback', async (req, res) => {
+  try {
+    const { opinion } = req.body;
 
-  feedbackData.push({ opinion });
+    // Salvar a avaliação no banco de dados
+    const feedback = await prisma.feedback.create({
+      data: {
+        opinion,
+      },
+    });
 
-  return res.status(200).json({ message: 'Feedback recebido com sucesso' });
-});
-
-
-app.post('/api/register', (req, res) => {
-    const { username, password } = req.body;
-
-  const existingUser = users.find((user) => user.username === username);
-  if (existingUser) {
-    return res.status(400).json({ message: 'Usuário já registrado' });
+    res.status(201).json(feedback);
+  } catch (error) {
+    console.error('Erro ao salvar a avaliação:', error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
   }
-
-  const newUser = { username, password };
-  users.push(newUser);
-
-  return res.status(201).json({ message: 'Usuário registrado com sucesso' });
 });
 
 app.post('/api/login', (req, res) => {
