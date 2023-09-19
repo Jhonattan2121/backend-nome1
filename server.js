f5b2b408fea7f34f8c33644b9a2bf6bc6d1d34e4
jhonattan ferri <jhoontutors@gmail.com>
2023-09-19 16:31:00 -0300

    76ty

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const User = require('./models/User'); 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,18 +10,43 @@ app.use(
   })
 );
 
-app.post('/api/register', (req, res) => {
-  const { username, password } = req.body;
+const pool = new Pool({
+  user: 'conta',         
+  host: 'localhost',       
+  database: 'mydb',        
+  password: '123',  
+  port: 5432,       
+});
 
-  const existingUser = users.find((user) => user.username === username);
-  if (existingUser) {
-    return res.status(400).json({ message: 'Usuário já registrado' });
+app.get('/api/users', (req, res) => {
+  dbInstance.any('SELECT * FROM users')
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Erro ao recuperar dados de usuários' });
+    });
+});
+
+
+router.post('/register', async (req, res) => {
+  const { username, password, email, firstName, lastName } = req.body;
+
+  const newUser = new User({
+    username,
+    password, 
+    email,
+    firstName,
+    lastName,
+  });
+
+  try {
+    await newUser.save();
+
+    res.status(201).json({ message: 'Usuário registrado com sucesso' });
+  } catch (error) {
+    res.status(500).json({ message: 'Ocorreu um erro ao registrar o usuário' });
   }
-
-  const newUser = { username, password };
-  users.push(newUser);
-
-  return res.status(201).json({ message: 'Usuário registrado com sucesso' });
 });
 
 app.post('/api/login', (req, res) => {
