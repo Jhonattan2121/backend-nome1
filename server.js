4f636d9ee18e0dca3f61c26ffea1b9508628a7fb
jhonattan ferri <jhoontutors@gmail.com>
2023-09-19 20:43:49 -0300

    123sq

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://projeto-front-end-login-e-cadastro.vercel.app',
+    origin: 'conectaamigos.vercel.app',
   }));
 // Simule um banco de dados de usuários (você deve usar um banco de dados real)
 const users = [];
