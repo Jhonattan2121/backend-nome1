3449c31e5789aaf0587f83a77c77587bc7fe9848
jhonattan ferri <jhoontutors@gmail.com>
2023-09-18 21:33:55 -0300

    435df

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const axios = require('axios');
+
 app.use(express.json());
 app.use(
   cors({
