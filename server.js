ee54ab06007aa7a3344ee94ce3b2c95eb46f154e
jhonattan ferri <jhoontutors@gmail.com>
2023-09-19 18:37:02 -0300

    0i95i

--- server.js
+++ server.js
@@ -1,51 +1,44 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; // Use a porta fornecida pelo ambiente ou 3000 por padrão
 const cors = require('cors');
-const { Pool } = require('pg');
-
 app.use(express.json());
-app.use(
-  cors({
+
+app.use(cors({
     origin: 'https://projeto-front-end-login-e-cadastro.vercel.app',
-  })
-);
+  }));
+// Simule um banco de dados de usuários (você deve usar um banco de dados real)
+const users = [];
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'mydb',
-  password: '123',
-  port: 5432,
-});
+// Rota de registro de usuário
+app.post('/api/register', (req, res) => {
+    const { username, password } = req.body;
 
-app.post('/api/register', async (req, res) => {
-  const { username, password, email } = req.body;
-  try {
-    const query = 'INSERT INTO users (username, password, email) VALUES ($1, $2, $3)';
-    await pool.query(query, [username, password, email]);
-    res.status(201).send('Usuário registrado com sucesso.');
-  } catch (error) {
-    console.error('Erro ao registrar usuário:', error);
-    res.status(500).send('Ocorreu um erro ao registrar o usuário.');
+  // Verifique se o usuário já existe
+  const existingUser = users.find((user) => user.username === username);
+  if (existingUser) {
+    return res.status(400).json({ message: 'Usuário já registrado' });
   }
+
+  // Crie um novo usuário (você deve criptografar a senha antes de armazená-la)
+  const newUser = { username, password };
+  users.push(newUser);
+
+  return res.status(201).json({ message: 'Usuário registrado com sucesso' });
 });
 
+// Rota de login de usuário
 app.post('/api/login', (req, res) => {
-  const { username, password } = req.body;
-  const query = 'SELECT * FROM users WHERE username = $1 AND password = $2';
-  pool.query(query, [username, password], (error, result) => {
-    if (error) {
-      console.error('Erro ao fazer login:', error);
-      res.status(500).json({ message: 'Ocorreu um erro ao fazer login' });
-    } else {
-      if (result.rows.length === 1) {
-        res.status(200).json({ message: 'Login bem-sucedido' });
-      } else {
-        res.status(401).json({ message: 'Credenciais inválidas' });
-      }
-    }
-  });
+    const { username, password } = req.body;
+
+  // Verifique se o usuário existe e a senha está correta (você deve verificar contra o banco de dados)
+  const user = users.find((user) => user.username === username);
+
+  if (!user || user.password !== password) {
+    return res.status(401).json({ message: 'Credenciais inválidas' });
+  }
+
+  return res.status(200).json({ message: 'Login bem-sucedido' });
 });
 
 app.listen(port, () => {
