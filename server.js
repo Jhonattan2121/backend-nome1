de426aa868ed9f948d6504007df87a3cf204441b
jhonattan ferri <jhoontutors@gmail.com>
2023-09-18 22:23:33 -0300

    3456dfg

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('./prisma/');
 const prisma = new PrismaClient();
 app.use(express.json());
 app.use(
