b016650400a578f5396f31f5af26dee06321a803
jhonattan ferri <jhoontutors@gmail.com>
2023-10-07 21:26:02 -0300

    mudancas gerais

---
+++ prisma/generated/client/edge.js
@@ -0,0 +1,186 @@
+
+Object.defineProperty(exports, "__esModule", { value: true });
+
+const {
+  PrismaClientKnownRequestError,
+  PrismaClientUnknownRequestError,
+  PrismaClientRustPanicError,
+  PrismaClientInitializationError,
+  PrismaClientValidationError,
+  NotFoundError,
+  getPrismaClient,
+  sqltag,
+  empty,
+  join,
+  raw,
+  Decimal,
+  Debug,
+  objectEnumValues,
+  makeStrictEnum,
+  Extensions,
+  warnOnce,
+  defineDmmfProperty,
+  Public,
+} = require('./runtime/edge')
+
+
+const Prisma = {}
+
+exports.Prisma = Prisma
+exports.$Enums = {}
+
+/**
+ * Prisma Client JS version: 5.4.1
+ * Query Engine version: 2f302df92bd8945e20ad4595a73def5b96afa54f
+ */
+Prisma.prismaVersion = {
+  client: "5.4.1",
+  engine: "2f302df92bd8945e20ad4595a73def5b96afa54f"
+}
+
+Prisma.PrismaClientKnownRequestError = PrismaClientKnownRequestError;
+Prisma.PrismaClientUnknownRequestError = PrismaClientUnknownRequestError
+Prisma.PrismaClientRustPanicError = PrismaClientRustPanicError
+Prisma.PrismaClientInitializationError = PrismaClientInitializationError
+Prisma.PrismaClientValidationError = PrismaClientValidationError
+Prisma.NotFoundError = NotFoundError
+Prisma.Decimal = Decimal
+
+/**
+ * Re-export of sql-template-tag
+ */
+Prisma.sql = sqltag
+Prisma.empty = empty
+Prisma.join = join
+Prisma.raw = raw
+Prisma.validator = Public.validator
+
+/**
+* Extensions
+*/
+Prisma.getExtensionContext = Extensions.getExtensionContext
+Prisma.defineExtension = Extensions.defineExtension
+
+/**
+ * Shorthand utilities for JSON filtering
+ */
+Prisma.DbNull = objectEnumValues.instances.DbNull
+Prisma.JsonNull = objectEnumValues.instances.JsonNull
+Prisma.AnyNull = objectEnumValues.instances.AnyNull
+
+Prisma.NullTypes = {
+  DbNull: objectEnumValues.classes.DbNull,
+  JsonNull: objectEnumValues.classes.JsonNull,
+  AnyNull: objectEnumValues.classes.AnyNull
+}
+
+
+
+/**
+ * Enums
+ */
+exports.Prisma.TransactionIsolationLevel = makeStrictEnum({
+  ReadUncommitted: 'ReadUncommitted',
+  ReadCommitted: 'ReadCommitted',
+  RepeatableRead: 'RepeatableRead',
+  Serializable: 'Serializable'
+});
+
+exports.Prisma.UserScalarFieldEnum = {
+  id: 'id',
+  email: 'email',
+  password: 'password',
+  photourl: 'photourl',
+  createdAt: 'createdAt'
+};
+
+exports.Prisma.SortOrder = {
+  asc: 'asc',
+  desc: 'desc'
+};
+
+exports.Prisma.QueryMode = {
+  default: 'default',
+  insensitive: 'insensitive'
+};
+
+exports.Prisma.NullsOrder = {
+  first: 'first',
+  last: 'last'
+};
+
+
+exports.Prisma.ModelName = {
+  User: 'User'
+};
+/**
+ * Create the Client
+ */
+const config = {
+  "generator": {
+    "name": "client",
+    "provider": {
+      "fromEnvVar": null,
+      "value": "prisma-client-js"
+    },
+    "output": {
+      "value": "D:\\todos meus projetos\\my-backend-app\\prisma\\generated\\client",
+      "fromEnvVar": null
+    },
+    "config": {
+      "engineType": "library"
+    },
+    "binaryTargets": [
+      {
+        "fromEnvVar": null,
+        "value": "windows",
+        "native": true
+      }
+    ],
+    "previewFeatures": [],
+    "isCustomOutput": true
+  },
+  "relativeEnvPaths": {
+    "rootEnvPath": "../../../.env",
+    "schemaEnvPath": "../../../.env"
+  },
+  "relativePath": "../..",
+  "clientVersion": "5.4.1",
+  "engineVersion": "2f302df92bd8945e20ad4595a73def5b96afa54f",
+  "datasourceNames": [
+    "db"
+  ],
+  "activeProvider": "postgresql",
+  "postinstall": false,
+  "inlineDatasources": {
+    "db": {
+      "url": {
+        "fromEnvVar": "DATABASE_URL",
+        "value": null
+      }
+    }
+  },
+  "inlineSchema": "Z2VuZXJhdG9yIGNsaWVudCB7CiAgcHJvdmlkZXIgPSAicHJpc21hLWNsaWVudC1qcyIKICBvdXRwdXQgICA9ICIuL2dlbmVyYXRlZC9jbGllbnQiCn0KCmRhdGFzb3VyY2UgZGIgewogIHByb3ZpZGVyID0gInBvc3RncmVzcWwiCiAgdXJsICAgICAgPSBlbnYoIkRBVEFCQVNFX1VSTCIpCn0KCm1vZGVsIFVzZXIgewogIGlkICAgICAgIFN0cmluZyAgICBAaWQKICBlbWFpbCAgICBTdHJpbmcgICAgQHVuaXF1ZQogIHBhc3N3b3JkIFN0cmluZwogIHBob3RvdXJsIFN0cmluZz8KICBjcmVhdGVkQXQgRGF0ZVRpbWUgQGRlZmF1bHQobm93KCkpIEBtYXAoImNyZWF0ZWRfYXQiKQoKfQoK",
+  "inlineSchemaHash": "b9b1d68858dc0cbfe7d62bd63020af22fcf73fb68302520bb8cff5773ee36c10",
+  "noEngine": false
+}
+config.dirname = '/'
+
+config.runtimeDataModel = JSON.parse("{\"models\":{\"User\":{\"dbName\":null,\"fields\":[{\"name\":\"id\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":true,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"email\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":true,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"password\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"photourl\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":false,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"createdAt\",\"dbName\":\"created_at\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"DateTime\",\"default\":{\"name\":\"now\",\"args\":[]},\"isGenerated\":false,\"isUpdatedAt\":false}],\"primaryKey\":null,\"uniqueFields\":[],\"uniqueIndexes\":[],\"isGenerated\":false}},\"enums\":{},\"types\":{}}")
+defineDmmfProperty(exports.Prisma, config.runtimeDataModel)
+
+
+config.injectableEdgeEnv = () => ({
+  parsed: {
+    DATABASE_URL: typeof globalThis !== 'undefined' && globalThis['DATABASE_URL'] || typeof process !== 'undefined' && process.env && process.env.DATABASE_URL || undefined
+  }
+})
+
+if (typeof globalThis !== 'undefined' && globalThis['DEBUG'] || typeof process !== 'undefined' && process.env && process.env.DEBUG || undefined) {
+  Debug.enable(typeof globalThis !== 'undefined' && globalThis['DEBUG'] || typeof process !== 'undefined' && process.env && process.env.DEBUG || undefined)
+}
+
+const PrismaClient = getPrismaClient(config)
+exports.PrismaClient = PrismaClient
+Object.assign(exports, Prisma)
+
--- server.js
+++ server.js
@@ -1,42 +1,107 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000; // Use a porta fornecida pelo ambiente ou 3000 por padrão
+const bodyParser = require('body-parser');
+const { PrismaClient } = require('./prisma/generated/client');
+const bcrypt = require('bcrypt');
 const cors = require('cors');
-app.use(express.json());
+const { v4: uuidv4 } = require('uuid');
 
-app.use(cors({
-  origin: 'https://conectaamigos.vercel.app',
-}));
-const users = [];
+const prisma = new PrismaClient();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.post('/api/register', (req, res) => {
-    const { username, password } = req.body;
+app.post('/signup', async (req, res) => {
+  try {
+    const { email, password, photourl } = req.body;
 
-  const existingUser = users.find((user) => user.username === username);
-  if (existingUser) {
-    return res.status(400).json({ message: 'Usuário já registrado' });
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ error: 'Email e senha são obrigatórios.' });
+    }
+
+    // Verifique se o usuário com o email fornecido já existe
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email já cadastrado.' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        id: uuidv4(), // Use UUID para gerar um ID único
+        email,
+        password: hashedPassword,
+        photourl,
+      },
+    });
+
+    res.status(201).json({ user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro no cadastro de usuário.' });
   }
-
-  const newUser = { username, password };
-  users.push(newUser);
-
-  return res.status(201).json({ message: 'Usuário registrado com sucesso' });
 });
 
-// Rota de login de usuário
-app.post('/api/login', (req, res) => {
-    const { username, password } = req.body;
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-  // Verifique se o usuário existe e a senha está correta (você deve verificar contra o banco de dados)
-  const user = users.find((user) => user.username === username);
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ error: 'Email e senha são obrigatórios.' });
+    }
 
-  if (!user || user.password !== password) {
-    return res.status(401).json({ message: 'Credenciais inválidas' });
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Credenciais inválidas.' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Credenciais inválidas.' });
+    }
+
+    // Responda com o ID do usuário
+    res.status(200).json({ id: user.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro no login.' });
   }
-
-  return res.status(200).json({ message: 'Login bem-sucedido' });
 });
 
-app.listen(port, () => {
-  console.log(`Servidor está rodando na porta ${port}`);
+
+app.get('/user/:userId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId }, // Procura um usuário pelo ID
+    });
+
+    if (!user) {
+      // Se o usuário não for encontrado, retorne um status 404 (Not Found).
+      return res.status(404).json({ error: 'Perfil do usuário não encontrado.' });
+    }
+
+    // Envie os dados do perfil como resposta.
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(`Erro ao buscar perfil do usuário: ${error.message}`);
+    res.status(500).json({ error: 'Erro ao buscar perfil do usuário.' });
+  }
+});
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
