cb9cc7a2a47b4ce8fbd3cad89659f8d6aed5cb40
jhonattan ferri <jhoontutors@gmail.com>
2023-10-10 16:19:28 -0300

    prisma

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('./prisma/generated/client');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 
