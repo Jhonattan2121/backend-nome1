cbc0aa188d2bab171d564ca4f1855d8e3b808436
jhonattan ferri <jhoontutors@gmail.com>
2023-10-10 13:35:35 -0300

    2323

--- server.js
+++ server.js
@@ -4,21 +4,11 @@ const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
-require('dotenv').config();
-
-
 const prisma = new PrismaClient();
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const corsOptions = {
-  origin: 'https://conectaamigos.vercel.app', 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-};
-
-app.use(cors(corsOptions));
 
 app.post('/signup', async (req, res) => {
   try {
