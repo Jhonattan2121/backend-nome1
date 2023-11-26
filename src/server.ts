1f88f1dc22bddee1f56f572760ccdaae0fabe0e0
jhonattan2121 <jhoontutors@gmail.com>
2023-11-26 01:40:22 -0300

    vercel banco de dados

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://conectaamigos.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
