945f514a21f20405c54bfce51aae6b07ba0dc134
jhonattan2121 <jhoontutors@gmail.com>
2023-11-26 02:04:54 -0300

    vercel banco de dados

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors({
-  origin: 'https://conectaamigos.vercel.app',
+  origin: '*',  
   credentials: true,
 }));app.use(bodyParser.json());
 
