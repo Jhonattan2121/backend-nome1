2c51d8ebd4232488001fd680235f06b7c13d668b
jhonattan2121 <jhoontutors@gmail.com>
2023-11-26 02:26:25 -0300

    vercel banco de dados

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const prisma = new PrismaClient({
 })
 const app = express();
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://conectaamigos.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors({
-  origin: '*',  
+  origin: 'https://conectaamigos.vercel.app',  
   credentials: true,
 }));app.use(bodyParser.json());
 
