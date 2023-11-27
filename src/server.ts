37c2b6c4bb42680c9f92927236e8e563ba65cb81
jhonattan2121 <jhoontutors@gmail.com>
2023-11-27 17:11:20 -0300

    vercel banco de dados

--- src/server.ts
+++ src/server.ts
@@ -24,38 +24,6 @@ interface UserIdParam {
   userId: string;
 }
 
-
-
-app.post('/signup', async (req: Request<{}, {}, UserRequestBody>, res: Response) => {
-  const { email, password } = req.body;
-
-  // Verificar se o e-mail já está cadastrado
-  const existingUser = await prisma.user.findUnique({
-    where: { email },
-  });
-
-  if (existingUser) {
-    return res.status(400).json({ error: 'E-mail já cadastrado.' });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  try {
-    const user = await prisma.user.create({
-      data: {
-        email,
-        password: hashedPassword,
-      },
-    });
-
-    res.status(201).json({ user });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro no cadastro de usuário.' });
-  }
-});
-
-
 app.post('/login', async (req: Request<{}, {}, UserRequestBody>, res: Response) => {
   const { email, password } = req.body;
 
@@ -101,6 +69,53 @@ app.get('/users', async (_req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao buscar usuários.' });
+  }
+});
+
+
+// Adicione esta rota antes das rotas de cadastro
+app.get('/userExists', async (req: Request<{ email: string }, {}, {}>, res: Response) => {
+  const { email } = req.query;
+
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { email: email as string },
+    });
+
+    res.json({ exists: !!existingUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao verificar a existência do e-mail.' });
+  }
+});
+
+// Rota /signup modificado
+app.post('/signup', async (req: Request<{}, {}, UserRequestBody>, res: Response) => {
+  const { email, password } = req.body;
+
+  // Verificar se o e-mail já está cadastrado
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    return res.status(400).json({ error: 'E-mail já cadastrado.' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({ user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro no cadastro de usuário.' });
   }
 });
 
