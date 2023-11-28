2684adffb8ba1517d0a5f1f3b6856d026cec4512
jhonattan2121 <jhoontutors@gmail.com>
2023-11-27 21:59:02 -0300

    vercel banco de dados

--- src/server.ts
+++ src/server.ts
@@ -14,41 +14,47 @@ app.use(cors({
 }));
 
 app.use(bodyParser.json());
-function generateToken(userId: string): string {
-  const secretKey = process.env.SECRET_KEY || 'your_secret_key'; // Replace with your actual secret key
-  const expiresIn = '1h'; // Set the token expiration time
 
-  const token = jwt.sign({ userId }, secretKey, { expiresIn });
-  return token;
-}
-
-// Middleware to verify JWT token
-function verifyToken(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers.authorization?.split(' ')[1];
-
-  if (!token) {
-    return res.status(401).json({ error: 'Token não fornecido.' });
-  }
-
-  const secretKey = process.env.SECRET_KEY || 'your_secret_key'; // Replace with your actual secret key
-
-  jwt.verify(token, secretKey, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ error: 'Token inválido.' });
-    }
-
-    (req as any).userId = (decoded as any).userId; // Attach userId to the request object
-    next();
-  });
-}
 
 interface UserRequestBody {
   email: string;
   password: string;
 }
 
-app.post('/signup', async (req: Request<{}, {}, UserRequestBody>, res: Response) => {
+const SECRET_KEY = 'Jhonattan';
+
+const generateToken = (userId: string ) => {
+  return jwt.sign({ id: userId }, SECRET_KEY, { expiresIn: '1h' });
+};
+
+const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.header('Authorization')?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err: any, decoded: any) => {
+    if (err) {
+      return res.status(403).json({ error: 'Token is not valid' });
+    }
+
+    if (!decoded || typeof decoded.id !== 'number') {
+      return res.status(403).json({ error: 'Invalid user ID in the token' });
+    }
+
+    (req as any).userId = +decoded.id;
+    next();
+  });
+};
+
+
+app.post('/Auth/signup', async (req: Request<{}, {}, UserRequestBody>, res: Response) => {
   const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).json({ msg: 'Email e senha são obrigatórios' });
+  }
 
   try {
     const existingUser = await prisma.user.findUnique({
@@ -56,26 +62,29 @@ app.post('/signup', async (req: Request<{}, {}, UserRequestBody>, res: Response)
     });
 
     if (existingUser) {
-      return res.status(400).json({ error: 'E-mail já registrado.' });
+      return res.status(409).json({ error: 'Email já cadastrado.' });
     }
+   
 
     const newUser = await prisma.user.create({
-      data: {
-        email,
+      data: { 
+        email, 
         password: await bcrypt.hash(password, 10),
+
       },
     });
 
-     const token = generateToken(newUser.id);
+    const token = generateToken(newUser.id);
 
-    res.status(201).json({ user: newUser , token  });
+
+    res.status(201).json({ user: newUser , token   });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro no cadastro de usuário.' });
   }
 });
 
-app.post('/login', async (req: Request<{}, {}, UserRequestBody>, res: Response) => {
+app.post('/Auth/login', async (req: Request<{}, {}, UserRequestBody>, res: Response) => {
   const { email, password } = req.body;
 
   try {
@@ -93,15 +102,16 @@ app.post('/login', async (req: Request<{}, {}, UserRequestBody>, res: Response) 
       return res.status(401).json({ error: 'Credenciais inválidas.' });
     }
 
+    const token = generateToken(user.id);
 
-    res.status(200).json({ id: user.id,  });
+    res.status(200).json({ id: user.id, token });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro no processo de login.' });
   }
 });
 
-app.get('/user/:userId', verifyToken, async (req: Request, res: Response) => {
+app.get('/user/:userId', authenticateJWT, async (req: Request, res: Response) => {
   try {
     const userId = (req as any).userId;
 
@@ -119,7 +129,7 @@ app.get('/user/:userId', verifyToken, async (req: Request, res: Response) => {
   }
 });
 
-app.get('/users', verifyToken, async (_req: Request, res: Response) => {
+app.get('/users', authenticateJWT, async (_req: Request, res: Response) => {
   try {
     const users = await prisma.user.findMany();
     res.json(users);
@@ -130,7 +140,7 @@ app.get('/users', verifyToken, async (_req: Request, res: Response) => {
 });
 
 
-const PORT = process.env.PORT || 3234;
+const PORT = parseInt(process.env.PORT || '3234', 10);
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
